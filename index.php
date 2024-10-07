40dc5c681f8900b9d72ddee4106c07f67898aa76
Pau Muñoz Serra <munozserrap@gmail.com>
2024-10-07 15:57:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Style/style.css">
+    <title>Practica 4</title>
+</head>
+<body>
+    <header>
+        <!-- Enlace a la izquierda para "Home" -->
+        <div class="home">
+            <a href="#">Home</a>
+        </div>
+
+        <!-- Perfil desplegable a la derecha -->
+        <div class="profile-dropdown">
+            <button class="profile-btn">Perfil</button>
+            <div class="dropdown-content">
+                <a href="#">Iniciar sesión</a>
+                <a href="#">Crear usuario</a>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
